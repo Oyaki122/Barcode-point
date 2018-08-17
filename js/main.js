499a3c38a3debceea4200d9dbbd0f38ee7e8db85
Oyaki122 <oc.yaki122@gmail.com>
2018-08-17 22:49:23 +0900

    複数個の読み込みに一応対応

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-var request = new XMLHttpRequest();
-
-
 function sender(result) {
     console.log(result);
     var point = parseInt($("#Value").val()) || 0;
 
-    if (result == 0) result = 1000;
-    request.open('GET', "https://script.google.com/macros/s/AKfycbz7GBmXEky8aCJCkbzz6FiWKWGWCT3lgbyywf7zLlpK9wfx-EU/exec?id=" + result + "&value=" + point + "&mode=true");
-    request.send("");
-    request.onreadystatechange = function () {
-        if (request.readyState != 4) {
-            $('.sendresult').text("Sending...");
-        } else if (request.status != 200) {
-            $('.sendresult').text("senderror");
-        } else {
+    var request = [];
 
-            var json = request.responseText;
-            var data = JSON.parse(json);
-            if (data.value == undefined) {
-                $('.sendresult').text("senderror");
+    var logarea = document.getElementById("log");
+
+    for (var i = 0; i < result.length; i++)(function (result) {
+
+        request[i] = new XMLHttpRequest();
+        request[i].num = result[i].Value;
+
+        request[i].logarea = logarea.appendChild(document.createElement("p"));
+
+
+        if (result == 0) result = 1000;
+        request[i].open('GET', "https://script.google.com/macros/s/AKfycbz7GBmXEky8aCJCkbzz6FiWKWGWCT3lgbyywf7zLlpK9wfx-EU/exec?id=" + result[i].Value + "&value=" + point + "&mode=true");
+        request[i].send("");
+        request[i].onreadystatechange = function () {
+            if (this.readyState != 4) {
+                this.logarea.innerHTML = "Sending...";
+            } else if (this.status != 200) {
+                this.logarea.innerHTML = "senderror";
             } else {
-                $('.sendresult').text("Success now " + data.value + "pt");
 
+                var json = this.responseText;
+                var data = JSON.parse(json);
+                if (data.value == undefined) {
+                    this.logarea.innerHTML = "senderror";
+                } else {
+                    this.logarea.innerHTML = "Success " + this.num + " " + data.value + "pt";
+
+                }
             }
-        }
-    };
+        };
+    }(result));
 }
 
 
@@ -44,7 +54,7 @@ $(function () {
             }
 
             Result.innerHTML = tempArray.join("<br />");
-            sender(result[0].Value);
+            sender(result);
         } else {
             if (result.length === 0) {
                 Result.innerHTML = "Decoding failed.";
